781f65313402fd52c158db9be98af5455bb1f58e
ikema19 <masa0208yuki@icloud.com>
2023-10-31 16:04:57 +0900

    pg

--- index.js
+++ index.js
@@ -1,26 +1,54 @@
 const express = require("express");
 const mysql = require('mysql');
 const app = express();
+const { Client } = require("pg");
 
-const connection = mysql.createConnection({
-  host: 'postgres://nodetest_db_user:CTW7UiWH7fzDAVdxqxMAZCzegPJ7aHQy@dpg-ckvgbi6b0mos739evjog-a/nodetest_db',
-  user: 'nodetest_db_user',
-  password: 'CTW7UiWH7fzDAVdxqxMAZCzegPJ7aHQy',
-  database: 'nodetest_db'
+//postgresql
+const client = new Client({
+  host: "dpg-cl09ib237rbc738kmpsg-a.oregon-postgres.render.com",
+  port: 5432,
+  database: "kirumo",
+  user: "kirumo",
+  password: "rUT0ctuPLQOFupoZTjxRUVn0blp7gnSp",
 });
 
 app.get("/", (req, res) => {
   res.send("Hello world\n");
 });
 
-app.get("/mysql", (req, res) => {
-	 connection.query(
-    'SELECT * FROM users',
-    (error, results) => {
-      console.log(results);
-      res.send(results);
+app.get("/pg", (req, res) => {
+  // //pg接続
+  // client.connect();
+
+  // //クエリの作成
+  // const query = {
+  //   text: "SELECT * FROM testTable",
+  // };
+
+  // //INSERTの場合
+  // //const query = {
+  // //  text: "INSERT INTO member VALUES ($1, $2)",
+  // //  values: [1, "山田太郎"],
+  // //};
+
+  // client
+  // .query(query)
+  // .then((res) => {
+  //   console.log(res);
+  //   client.end();
+  // })
+  // .catch((e) => console.error(e.stack));
+
+  client.query('SELECT * FROM testTable', (error, results) => {
+    if (error) {
+      console.error('クエリエラー:', error);
+      res.status(500).json({ error: 'データベースエラー' });
+    } else {
+      res.json(results.rows);
     }
-  );
+  });
+
+
 });
 
 app.listen(3000);
